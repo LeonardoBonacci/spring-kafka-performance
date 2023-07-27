45fd5cef679a0bd6fa4d934269fe4c586c964488
Jeffrey van Helden <jeffrey.van.helden@nzta.govt.nz>
2023-07-27 15:39:53 +1200

    first setup

---
+++ performance/src/main/java/guru/bonacci/performance/Output.java
@@ -0,0 +1,17 @@
+package guru.bonacci.performance;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Output {
+	
+	private String foo;
+	private Integer goo;
+	private Double bar;
+	private Boolean baz;
+	private Long when;
+}
---
+++ performance/src/test/java/guru/bonacci/kafka/performance/PerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.bonacci.kafka.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
